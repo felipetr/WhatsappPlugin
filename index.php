98690e52fca8d6dfbb17e110d487289b31e846b2
Felipe Travassos <contato@felipetravassos.com>
2024-01-09 17:53:14 -0300

    MVP

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package WhatsAppPlugin
- * @version 0.5
+ * @version 0.5.1
  */
 /*
 Plugin Name: WhatsAppPlugin
 Plugin URI: https://github.com/felipetr/WhatsappPlugin
 Description: Plugin WordPress de Link Flutuante de Whatsapp
 Author: Felipe Travassos
-Version: 0.
+Version: 0.5.1
 Author URI: https://github.com/felipetr/
 */
 
